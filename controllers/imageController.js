05a7dad0eecf62982789c8a285b6c27c1181ac13
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-13 00:46:56 +1100

    add try catch to catch the error

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -4,9 +4,14 @@ const sharp=require('sharp');
 
 const displayImage= async(req,res)=>{
     const {filename, width, height}=req.query;
+    try{
     const image= await fsPromise.readFile(path.join(__dirname, "..","images", `${filename}.jpg`));
     const resizedImage= await sharp(image).resize(parseInt(width), parseInt(height)).toBuffer();
     res.contentType('image/jpeg');
     res.send(resizedImage);
+    }
+    catch(error){
+        res.status(500).send("Uncaught error occur: "+ error);
+    }
 }
 module.exports={displayImage}
